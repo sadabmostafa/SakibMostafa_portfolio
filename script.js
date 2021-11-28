4dc8e99e60828a46689f4b3846125bc9e9f3fe73
Sadab Mostafa <sadabmostafa@gmail.com>
2021-11-28 17:38:06 +0600

    version2

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Researcher","Research Assistant ","Lecturer", "Technical Paper Reviewer"],
+        strings: ["Researcher","Lecturer", "Software Developer", "Data Analysist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Researcher","Research Assistant ","Lecturer", "Technical Paper Reviewer"],
+        strings: ["Researcher","Lecturer", "Software Developer", "Data Analysist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
